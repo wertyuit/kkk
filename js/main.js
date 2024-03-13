33936e14464b8ff38ee4b9c179b3f8cc60d9cf5f
wertyuit <159531681+wertyuit@users.noreply.github.com>
2024-03-13 18:07:48 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//* Add PayPal Email
+simpleCart({
+  checkout: {
+    type: "PayPal",
+    email: "soufianesidki20@g"
+  }
+});
+
+//* Add shopping cart dropdown in header
+jQuery(document).ready(function () {
+  $('.showCart').on('click', function () {
+    $('#cartPopover').slideToggle('fast');
+    $('.showCart span.dropdown').toggleClass('fa-chevron-circle-down fa-chevron-circle-up');
+  })
+});
+
+//* Define spreadsheet URL (make sure you add the #gid=0 for the sheet you want to use)
+var googleSheetURI = 'https://docs.google.com/spreadsheets/d/1yJPGODwI4mRTItdqzfod8CpUjLc0lBLsI1JXK5dXlzk/edit?usp=sharing';
+
+//* Compile the Handlebars template for HR leaders
+var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+//* Load products from spreadsheet
+$('#products').sheetrock({
+  url: googleSheetURI,
+  query: "select A,B,C,D,E",
+  rowTemplate: HRTemplate
+});
